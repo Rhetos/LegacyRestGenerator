7c3d0f10ad0a8ad7ece7d7a4ccfde8c73026e3d9
Bojan Antolovic <bojan.antolovic@gmail.com>
2014-05-21 15:52:56 +0200

    Release 1.5.

--- CopyPlugins.bat
+++ CopyPlugins.bat
@@ -1,11 +1,11 @@
 ECHO Target folder = [%1]
 ECHO $(ConfigurationName) = [%2]
 
-SET ThisScriptFolder="%~dp0"
+REM "%~dp0" is this script's folder.
 
-XCOPY /Y/D/R %ThisScriptFolder%Plugins\Rhetos.LegacyRestGenerator\bin\%2\Rhetos.LegacyRestGenerator.dll %1 || EXIT /B 1
-XCOPY /Y/D/R %ThisScriptFolder%Plugins\Rhetos.LegacyRestGenerator\bin\%2\Rhetos.LegacyRestGenerator.pdb %1 || EXIT /B 1
-XCOPY /Y/D/R %ThisScriptFolder%Plugins\Rhetos.LegacyRestGenerator.DefaultConcepts\bin\%2\Rhetos.LegacyRestGenerator.DefaultConcepts.dll %1 || EXIT /B 1
-XCOPY /Y/D/R %ThisScriptFolder%Plugins\Rhetos.LegacyRestGenerator.DefaultConcepts\bin\%2\Rhetos.LegacyRestGenerator.DefaultConcepts.pdb %1 || EXIT /B 1
+XCOPY /Y/D/R "%~dp0"Plugins\Rhetos.LegacyRestGenerator\bin\%2\Rhetos.LegacyRestGenerator.dll %1 || EXIT /B 1
+XCOPY /Y/D/R "%~dp0"Plugins\Rhetos.LegacyRestGenerator\bin\%2\Rhetos.LegacyRestGenerator.pdb %1 || EXIT /B 1
+XCOPY /Y/D/R "%~dp0"Plugins\Rhetos.LegacyRestGenerator.DefaultConcepts\bin\%2\Rhetos.LegacyRestGenerator.DefaultConcepts.dll %1 || EXIT /B 1
+XCOPY /Y/D/R "%~dp0"Plugins\Rhetos.LegacyRestGenerator.DefaultConcepts\bin\%2\Rhetos.LegacyRestGenerator.DefaultConcepts.pdb %1 || EXIT /B 1
 
 EXIT /B 0
